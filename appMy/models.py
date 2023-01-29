edebfeaca052c6882a192caf0cff9bbc78303aba
Berkay Ülger <berkayulger45920@gmail.com>
2023-01-29 20:49:08 +0300

    -

--- appMy/models.py
+++ appMy/models.py
@@ -7,15 +7,23 @@ from django.contrib.auth.models import User
 class Brand(models.Model):
     title = models.CharField(("Marka"), max_length=50)
 
+    def __str__(self):
+        return self.title
+    
 class Category(models.Model):
     title = models.CharField(("Kategori"), max_length=50)
     
+    def __str__(self):
+        return self.title
 class Comment(models.Model):
     product = models.ForeignKey("appMy.Product", verbose_name=("Yorum Yapılan Ürün"), on_delete=models.CASCADE)
     user = models.ForeignKey(User, verbose_name=("Yorum Yapan Kullanıcı"), on_delete=models.CASCADE)
     title = models.CharField(("Yorum Başlığı"), max_length=50)
     text = models.TextField(("Yorum"))
     date_now = models.DateTimeField(("Yorum Yapılma Zamanı"), auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
 
 class Product(models.Model):
     brand = models.ForeignKey(Brand, verbose_name=("Ürün Markası"), on_delete=models.CASCADE)
@@ -23,4 +31,7 @@ class Product(models.Model):
     title = models.CharField(("Ürün Başlık"), max_length=50)
     text = models.TextField(("Ürün Özellikleri"))
     price = models.FloatField(("Ürün Fiyatı"))
+    image = models.FileField(("Ürün Fotoğrafı"), upload_to='', max_length=100, null=True)
 
+    def __str__(self):
+        return self.title
--- appMy/views.py
+++ appMy/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    context={}
+    context={"title":"Anasayfa"}
+    products1 =Product.objects.all().order_by("?")[:3]
+    category_e = Category.objects.get(title="Elektronik") 
+    products_e = Product.objects.filter(category=category_e)
+    
+    context.update({
+        "products1": products1,
+        "products_e": products_e,
+    })
     return render(request,'index.html',context)
 
 def productCategory(request):
@@ -12,7 +20,11 @@ def productCategory(request):
     return render(request,'category.html',context)
 
 def productAll(request):
-    context={}
+    context={"title": "Tüm Ürünler"}
+    
+    products = Product.objects.all()
+    
+    context.update({"products": products})
     return render(request,'products.html',context)
 
 def Clients(request):
