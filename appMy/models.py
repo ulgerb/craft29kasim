e07fc9ed2c4d45e3f96f60c0cccc1012fc552717
Berkay Ülger <berkayulger45920@gmail.com>
2023-01-29 20:00:29 +0300

    -

--- appMy/models.py
+++ appMy/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Brand(models.Model):
+    title = models.CharField(("Marka"), max_length=50)
+
+class Category(models.Model):
+    title = models.CharField(("Kategori"), max_length=50)
+    
+class Comment(models.Model):
+    product = models.ForeignKey("appMy.Product", verbose_name=("Yorum Yapılan Ürün"), on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name=("Yorum Yapan Kullanıcı"), on_delete=models.CASCADE)
+    title = models.CharField(("Yorum Başlığı"), max_length=50)
+    text = models.TextField(("Yorum"))
+    date_now = models.DateTimeField(("Yorum Yapılma Zamanı"), auto_now_add=True)
+
+class Product(models.Model):
+    brand = models.ForeignKey(Brand, verbose_name=("Ürün Markası"), on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name=("Ürün Kategorisi"), on_delete=models.CASCADE)
+    title = models.CharField(("Ürün Başlık"), max_length=50)
+    text = models.TextField(("Ürün Özellikleri"))
+    price = models.FloatField(("Ürün Fiyatı"))
+
